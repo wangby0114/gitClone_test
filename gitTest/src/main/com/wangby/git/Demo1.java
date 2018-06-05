762095e18ba53f35b90f54a00b89bba452a6d523
wangby <710697309@qq.com>
2018-06-05 23:36:34 +0800

    create

---
+++ gitTest/src/main/com/wangby/git/Demo1.java
@@ -0,0 +1,10 @@
+package main.com.wangby.git;
+
+/**
+ * Created by wangby on 2018/6/5.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
